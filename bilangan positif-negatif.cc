aac8a7cdc953c62b112a8854f2dc002c491d2fc8
SyahriRahmat <45659210+SyahriRahmat@users.noreply.github.com>
2018-12-08 21:40:05 +0700

    Add files via upload

---
+++ bilangan positif-negatif.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main () {
+int angka;
+
+    cout<<"Masukkan Angka Yang Anda Inginkan = ";
+    cin>>angka;
+
+    if (angka > 0)
+    cout<<angka<<" Maka Angka Tersebut Bernilai Positif";
+    else if (angka < 0)
+    {
+    cout<<angka<<" Maka Angka Tersebut Bernilai Negatif";
+    }
+}
